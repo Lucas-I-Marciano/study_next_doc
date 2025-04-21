890729f9006745b1aa130749eec5c3b8aaca0e64
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-21 15:42:08 -0300

    style: import and apply Inter font from next/font/google in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
-import '@/app/ui/global.css'
+import "@/app/ui/global.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.className} antialiased`}>
       <body>{children}</body>
     </html>
   );
