4bedff1bb6a77027ca1753f275356867a9d63dc8
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-05-19 20:13:12 -0300

    feat(ui/search): implement live search functionality

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,17 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const path = usePathname();
+  const router = useRouter();
+  const params = useSearchParams();
+  const page = params.get("page") || "1";
+
+  const handleSearch = (term: string) => {
+    router.replace(`${path}?query=${term}&page=${page}`);
+  };
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +20,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
